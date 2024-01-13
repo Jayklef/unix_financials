8a84c0316aecda8b12ca4cb839821cc3cec8d1a9
Jayklef <jerryarhawho@gmail.com>
2024-01-13 14:40:12 +0000

    Mapper/email config

--- src/main/java/com/jayklef/unix_financials/mapper/CustomMapper.java
+++ src/main/java/com/jayklef/unix_financials/mapper/CustomMapper.java
@@ -2,10 +2,20 @@ package com.jayklef.unix_financials.mapper;
 
 import com.jayklef.unix_financials.dto.AdminDto;
 import com.jayklef.unix_financials.entity.UserCred;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomMapper {
 
     public AdminDto mapToDto(UserCred userCred){
-        return AdminDto.builder().build();
+
+        return AdminDto.builder()
+                .id(userCred.getId())
+                .firstname(userCred.getFirstName())
+                .lastname(userCred.getLastName())
+                .email(userCred.getEmail())
+                .team(userCred.getTeam().toString())
+                .role(userCred.getRole().toString())
+                .build();
     }
 }
--- src/main/java/com/jayklef/unix_financials/service/Impl/EmailServiceImpl.java
+++ src/main/java/com/jayklef/unix_financials/service/Impl/EmailServiceImpl.java
@@ -4,6 +4,7 @@ import com.jayklef.unix_financials.dto.EmailDetails;
 import com.jayklef.unix_financials.service.EmailService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -15,12 +16,15 @@ public class EmailServiceImpl implements EmailService {
 
     private JavaMailSender javaMailSender;
 
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
     @Override
     public void sendEmail(EmailDetails emailDetails) {
 
         try {
             SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setFrom("");
+            mailMessage.setFrom(senderEmail);
             mailMessage.setTo(emailDetails.getRecipient());
             mailMessage.setSubject(emailDetails.getSubject());
             mailMessage.setCc(emailDetails.getRecipientGroup());
